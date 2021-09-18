c0d4d9091b43c896dae870cb2db7cc363b3666f7
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-09-18 13:46:28 +0530

    connection to db

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,13 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import * as dotenv from 'dotenv';
 
+//db
+import { connectDB } from './Config/db.js';
+
+// routes
 import { homeRouter } from './Route/index.js';
+
+// middlewares
 import { errorHandler, notFoundHandler } from './Middleware/index.js';
 
 dotenv.config();
@@ -13,6 +19,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const environment = process.env.NODE_ENV || 'development';
+
+// db connect
+await connectDB();
 
 app.use(cors());
 app.use(helmet());
